a26c83ee8559a3b91989462752456580a0d9fa8c
netthread3@gmail.com <netthread3@gmail.com>
2024-08-26 15:34:43 +0300

    added basic courousel

--- src/app/sigalo/sigalo.component.ts
+++ src/app/sigalo/sigalo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-sigalo',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./sigalo.component.scss']
 })
 export class SigaloComponent {
+ images:string[] = ["gallo-hem.jpeg" , "marans.jpeg","gallo-hem.jpeg"  , "marans.jpeg"]
+ selectedIndex = signal(0)
+ selected = computed(() => this.images[this.selectedIndex()]) 
+
+ select(index:number){
+  this.selectedIndex.update(() => index)
+ }
+
+ selectNext(){
+  this.selectedIndex.update((index) => {
+    if(index === this.images.length-1){
+     return 0; // go to start
+    }
+
+    return index + 1 
+  })
+ }
+
+ selectPrev(){
+  this.selectedIndex.update((index) => {
+    if(index === 0){
+      return this.images.length - 1// go to last
+    }
+    return index - 1
+  })
+ }
 
 }
